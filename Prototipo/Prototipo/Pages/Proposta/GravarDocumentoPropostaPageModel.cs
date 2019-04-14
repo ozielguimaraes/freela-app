3698ff6bc04196fccab5aebfc9a2ecc525cc986f
Oziel Guimaraes de Paula Silva <contato@ozielguimaraes.net>
2019-04-13 21:39:22 -0300

    Melhorando teclado para fields comprador

---
+++ Prototipo/Prototipo/Pages/Proposta/GravarDocumentoPropostaPageModel.cs
@@ -0,0 +1,18 @@
+﻿using Prototipo.ViewModels;
+using System.Collections.ObjectModel;
+
+namespace Prototipo.Pages.Proposta
+{
+    public class GravarDocumentoPropostaPageModel : BasePageModel
+    {
+        public ObservableCollection<Models.Proposta> Items { get; set; }
+
+        public GravarDocumentoPropostaPageModel()
+        {
+            Items = new ObservableCollection<Models.Proposta>
+            {
+                new Models.Proposta{ }
+            };
+        }
+    }
+}
