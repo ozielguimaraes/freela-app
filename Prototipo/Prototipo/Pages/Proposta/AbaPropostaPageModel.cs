554732ca193736dbb12fe47c1314b4eba95c131b
Oziel Guimaraes de Paula Silva <contato@ozielguimaraes.net>
2019-05-02 20:37:26 -0300

    Adicionando novas chaves

--- Prototipo/Prototipo/Pages/Proposta/AbaPropostaPageModel.cs
+++ Prototipo/Prototipo/Pages/Proposta/AbaPropostaPageModel.cs
@@ -11,7 +11,17 @@ namespace Prototipo.Pages.Proposta
 {
     public class AbaPropostaPageModel : BasePageModel
     {
+        private readonly List<PreChaveVm> items;
+
         public ICommand LoadPreChavesCommand { get; set; }
+        public ICommand NovaPreChaveCommand { get; set; }
+
+        private PreChaveVm novaPreChave;
+        public PreChaveVm NovaPreChave
+        {
+            get { return novaPreChave; }
+            set { SetProperty(ref novaPreChave, value); }
+        }
 
         private ObservableCollection<PreChaveVm> preChaves;
         public ObservableCollection<PreChaveVm> PreChaves
@@ -22,8 +32,38 @@ namespace Prototipo.Pages.Proposta
 
         public AbaPropostaPageModel()
         {
+            NovaPreChave = new PreChaveVm();
             PreChaves = new ObservableCollection<PreChaveVm>();
             LoadPreChavesCommand = new Command(async () => await LoadPreChaves());
+            NovaPreChaveCommand = new Command(async () => await AdicionarNovaPreChave());
+
+            items = new List<PreChaveVm>
+                {
+                    //new PreChaveVm
+                    //{
+                    //    QuantidadeParcelas = 86,
+                    //    Valor = 40908,
+                    //    PrimeiroVencimento = DateTime.Now
+                    //},
+                    //new PreChaveVm
+                    //{
+                    //    QuantidadeParcelas = 86,
+                    //    Valor = 40908,
+                    //    PrimeiroVencimento = DateTime.Now
+                    //},
+                    //new PreChaveVm
+                    //{
+                    //    QuantidadeParcelas = 86,
+                    //    Valor = 40908,
+                    //    PrimeiroVencimento = DateTime.Now
+                    //},
+                    new PreChaveVm
+                    {
+                        QuantidadeParcelas = 180,
+                        Valor = 180000,
+                        PrimeiroVencimento = DateTime.Now
+                    }
+                };
         }
 
         private async Task LoadPreChaves()
@@ -35,38 +75,43 @@ namespace Prototipo.Pages.Proposta
             try
             {
                 PreChaves.Clear();
-                var items = new List<PreChaveVm>
-                {
-                    new PreChaveVm
-                    {
-                        QuantidadeParcelas = 86,
-                        Valor = 40908,
-                        PrimeiroVencimento = DateTime.Now
-                    },
-                    new PreChaveVm
-                    {
-                        QuantidadeParcelas = 86,
-                        Valor = 40908,
-                        PrimeiroVencimento = DateTime.Now
-                    },
-                    new PreChaveVm
-                    {
-                        QuantidadeParcelas = 86,
-                        Valor = 40908,
-                        PrimeiroVencimento = DateTime.Now
-                    },
-                    new PreChaveVm
-                    {
-                        QuantidadeParcelas = 180,
-                        Valor = 180000,
-                        PrimeiroVencimento = DateTime.Now
-                    }
-                };
 
                 foreach (var item in items)
                 {
                     PreChaves.Add(item);
                 }
+
+                await Task.Delay(500);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        private async Task AdicionarNovaPreChave()
+        {
+            if (IsBusy) return;
+
+            IsBusy = true;
+
+            try
+            {
+                if (NovaPreChave.QuantidadeParcelas <= 0)
+                {
+                    await MessageService.ShowAsync("Campo quantidade é obrigatório");
+                    return;
+                }
+                if (NovaPreChave.Valor <= 0)
+                {
+                    await MessageService.ShowAsync("Campo valor é obrigatório");
+                    return;
+                }
+                PreChaves.Add(NovaPreChave);
 
                 await Task.Delay(500);
             }
