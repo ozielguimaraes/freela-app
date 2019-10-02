bc181392cf2f386a9839e5a1f9a4a04acc8d1295
Oziel Guimarães de Paula Silva <contato@ozielguimaraes.net>
2019-10-02 17:36:56 -0300

    Adding xd document

---
+++ Prototipo/Prototipo/Exceptions/ServiceAuthenticationException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Prototipo.Exceptions
+{
+    public class ServiceAuthenticationException : Exception
+    {
+        public string Content { get; set; }
+
+        public ServiceAuthenticationException(string content)
+        {
+            Content = content;
+        }
+    }
+}
